9b9c064931ae4e31e6e300046e3fbe46068831fa
josue <josue@LAPTOP-5NHNEHLO>
2018-05-07 09:09:48 -0300

    ejercicio 5. realizado form bean de 2 maneras para tratar de solucionar el problema pero sin resultado.por josue487

---
+++ src/java/pto5/aplicacion/controlador/beans/forms/ProfesorFormBean.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pto5.aplicacion.controlador.beans.forms;
+
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import pto5.aplicacion.modelo.dominio.Profesor;
+
+/**
+ *
+ * @author josue
+ */
+@ManagedBean
+@ViewScoped
+public class ProfesorFormBean implements Serializable{
+    
+    private String apellido;
+    private String nombre;
+    private int legajo;
+    
+    private Profesor profesor;
+
+    /**
+     * Creates a new instance of ProfesorFormBean
+     */
+    public ProfesorFormBean() {
+        profesor = new Profesor();
+        
+    }
+
+    /**
+     * @return the profesor
+     */
+    public Profesor getProfesor() {
+        return profesor;
+    }
+
+    /**
+     * @param profesor the profesor to set
+     */
+    public void setProfesor(Profesor profesor) {
+        this.profesor = profesor;
+    }
+
+    /**
+     * @return the apellido
+     */
+    public String getApellido() {
+        return apellido;
+    }
+
+    /**
+     * @param apellido the apellido to set
+     */
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the legajo
+     */
+    public int getLegajo() {
+        return legajo;
+    }
+
+    /**
+     * @param legajo the legajo to set
+     */
+    public void setLegajo(int legajo) {
+        this.legajo = legajo;
+    }
+
+    
+}
