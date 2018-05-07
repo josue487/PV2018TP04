ba54207905ac203a6d68a82ebfe47559ad43acd1
josue <josue@LAPTOP-5NHNEHLO>
2018-05-06 22:44:10 -0300

    ejercicio 2. realizando el objeto o clase numero por josue487

---
+++ src/java/pto2/aplicacion/modelo/dominio/Numero.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pto2.aplicacion.modelo.dominio;
+
+/**
+ *
+ * @author josue
+ */
+public class Numero {
+    public double valor;
+
+    /**
+     * @return the valor
+     */
+    public double getValor() {
+        return valor;
+    }
+
+    /**
+     * @param valor the valor to set
+     */
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+}
